4522e4ade466ab6fb3750b5ab1d4c557cb109229
hak-fe <71654651+hak-fe@users.noreply.github.com>
2020-09-26 17:21:01 +0900

    두 수를 입력받아 두 수의 합을 구함

---
+++ 두 수를 입력받아 덧셈하기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int fir_input_num, sec_input_num;
+
+	scanf_s("%d%d", &fir_input_num, &sec_input_num);
+	
+	printf("%d", fir_input_num + sec_input_num);
+
+	return 0;
+}
