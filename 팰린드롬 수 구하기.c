a8eaf73d3ef8e8ff5a10fa3ff2554cce20e039d4
hak-fe <71654651+hak-fe@users.noreply.github.com>
2020-10-06 21:19:21 +0900

    Update 팰린드롬 수 구하기.c

--- 팰린드롬 수 구하기.c
+++ 팰린드롬 수 구하기.c
@@ -1,70 +1,69 @@
 #include <stdio.h>
 #include <math.h>
 
-	int main() {
-
-		int input, length = 0, flag = 0, repos1 = 0, repos2 = 0;
+	int main() 
+	{
+		int input, flag = 0, repos1 = 0, repos2 = 0;
 
 		do {
-			scanf_s("%d", &input);
+			int length = 0;
+			scanf("%d", &input);
 			if (input > 0 && input < 10)
 			{
 				printf("yes\n");
 				flag = 1;
 			}
-			else if (input != 0) {											// 자릿수 구하는 과정 ~ 시작 / 0이 아닐경우 시작
-				flag = 1;												//flag변수로 반복문 진행 여부 판단
+			else if (input != 0)
+			{											// 자릿수 구하는 과정 ~ 시작 / 0이 아닐경우 시작
+				flag = 1;													//flag변수로 반복문 진행 여부 판단
 				for (int div = 1;; div *= 10)
 				{
-					if (div <= input && input / div != 0)
+					if (div <= input && input / div != 0)				
 					{
 						length++;
 					}
-					else {
+					else
+					{
 						break;
 					}
 				}														// 자리수 구하기 끝 (성공 확인)
-				
-				if (length % 2 == 1) {
+				if (length % 2 == 1)
+				{
 					repos1 = input / pow(10, (length + 1) / 2);
 					repos2 = input % (int)pow(10, length / 2);
-					if (length > 3) {
+					if (length > 3)
+					{
 						repos1 = (repos1 / 10) + (repos1 % 10 * 10);
 					}
-					if (repos1 == repos2 && input >= 10) {
+					if (repos1 == repos2 && input >= 10)
+					{
 						printf("yes\n");
-						repos2 = 0;
-						repos1 = 0;
-						length = 0;
 					}
-					else {
+					else
+					{
 						printf("no\n");
-						repos1 = 0;
-						repos2 = 0;
-						length = 0;
 					}
 				}
-				else if (length % 2 == 0) {
+				else if (length % 2 == 0) 
+				{
 					repos1 = input / pow(10, length / 2);
 					repos2 = input % (int)pow(10, length / 2);
-					if (length > 2) {
+					if (length > 2)
+					{
 						repos1 = (repos1 / 10) + (repos1 % 10 * 10);
 					}
-					if (repos1 == repos2) {
+					if (repos1 == repos2) 
+					{
 						printf("yes\n");
-						repos2 = 0;
-						repos1 = 0;
-						length = 0;
 					}
-					else {
+					else 
+					{
 						printf("no\n");
-						repos1 = 0;
-						repos2 = 0;
-						length = 0;
 					}
 				}
 			}
-			else{                                        //0이 입력되면 바로 종료
+			else
+			{										// 0이 입력되면 바로 종료
 				flag = 0;
 			}
 		} while (flag);
