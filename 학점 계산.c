f5f1f3ae79240dc4bf9ee382519bcf982e3a7fa8
hak-fe <71654651+hak-fe@users.noreply.github.com>
2020-10-10 01:48:08 +0900

    학점을 입력받아, 평점을 계산하는 프로그램

---
+++ 학점 계산.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+	char grade1;
+	char grade2;
+	double score = 0;
+	scanf("%c%c", &grade1,&grade2);
+
+	switch (grade1)
+	{
+	case 'A':
+		score = 4.0;
+		if (grade2 == '+')
+			score += 0.3;
+		else if (grade2 == '0')
+			score = score;
+		else
+			score -= 0.3;
+		printf("%.1lf", score);
+		break;
+	case 'B':
+		score = 3.0;
+		if (grade2 == '+')
+			score += 0.3;
+		else if (grade2 == '0')
+			score = score;
+		else
+			score -= 0.3;
+		printf("%.1lf", score);
+		break;
+	case 'C':
+		score = 2.0;
+		if (grade2 == '+')
+			score += 0.3;
+		else if (grade2 == '0')
+			score = score;
+		else
+			score -= 0.3;
+		printf("%.1lf", score);
+		break;
+	case 'D':
+		score = 1.0;
+		if (grade2 == '+')
+			score += 0.3;
+		else if (grade2 == '0')
+			score = score;
+		else
+			score -= 0.3;
+		printf("%.1lf", score);
+		break;
+
+	default:
+		printf("0.0");
+		break;
+	}
+	
+	return 0;
+	}
